4743f74e84764267650aac66dcfe931efd618d1a
carlaldenbgo <carlaldenbgo@gmail.com>
2023-07-18 21:00:15 +0800

    Include continue/quit feature

--- Char-Array.cpp
+++ Char-Array.cpp
@@ -2,45 +2,49 @@
 #include <string>
 using namespace std;
 
-int main() 
+int main()
 {
-    int char_code;
-    string name;
-    cout << "What's your name?\n";
-    cin >> name;
-    cout << name << endl;
-	string namehashtag = name;
-    for (int i = 1; i < namehashtag.size(); i++) 
-    {
-        namehashtag = namehashtag.replace(i,1,"#");
-    }
-    cout << namehashtag << endl;
-    
-	
-	for (int j = 0; j < name.size(); j++)
-	{
-		char_code = int(name[j]);
+	int char_code;
+	string name;
+	string choice;
 
-		if (char_code == 122)
+	do
+	{
+		cout << "What's your name?\n";
+		cin >> name;
+		cout << name << endl;
+		string namehashtag = name;
+		for (int i = 1; i < namehashtag.size(); i++)
 		{
-			name[j] = char(97);
+			namehashtag = namehashtag.replace(i, 1, "#");
 		}
-		else if (char_code == 90)
+		cout << namehashtag << endl;
+
+		for (int j = 0; j < name.size(); j++)
 		{
-			name[j] = char(65);
+			char_code = int(name[j]);
+
+			if (char_code == 122)
+			{
+				name[j] = char(97);
+			}
+			else if (char_code == 90)
+			{
+				name[j] = char(65);
+			}
+			else if (char_code >= 65 && char_code <= 90 || char_code >= 97 && char_code <= 122)
+			{
+				name[j] = char(char_code + 1);
+			}
+			else if (char_code >= 33 && char_code <= 47)
+			{
+				name[j] = name[j];
+			}
 		}
-		else if (char_code >= 65 && char_code <= 90 || char_code >= 97 && char_code <= 122)
-		{
-			name[j] = char(char_code + 1);
-		}
-		else if (char_code >= 33 && char_code <= 47 )
-		{
-			name[j] = name[j];
-		}
-	
-	}
-    cout << name << endl;
-    
-    
-return 0;
+		cout << name << endl;
+		cout << "Would you like to continue? (y/n)" << endl;
+		cin >> choice;
+	} while (choice == "Y" || choice == "y");
+
+	return 0;
 }
